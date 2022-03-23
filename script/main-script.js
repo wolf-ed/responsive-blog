a3bb26848b5b456e907a3f5a35dc59e7560206b7
wolf-ed <wolf7ed@gmail.com>
2022-03-23 01:24:11 +0100

    carousel

--- script/main-script.js
+++ script/main-script.js
@@ -7,4 +7,14 @@ $(document).ready(() => {
         $nav.toggleClass("collapse");
     })
 
+    // owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    })
+
 })
